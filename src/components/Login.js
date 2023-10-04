348a695477920ebdf6e0bbe75363fb41aa1ca0c7
nayandrakshapally <nayan.drakshapally@gmail.com>
2023-10-04 09:39:48 -0400

    signup jsx changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 
 const Login = () => {
+  const [isSigninForm, setIsSigninForm] = useState(true);
+  const toggleSignInForm = () => {
+    setIsSigninForm(!isSigninForm);
+  };
   return (
     <div>
       <Header />
@@ -13,7 +17,14 @@ const Login = () => {
       </div>
       <div className="flex justify-center text-white">
         <form className="w-4/12 absolute p-16 bg-black my-44 rounded-lg bg-opacity-80">
-          <h1 className="text-4xl py-4 font-medium">Sign In</h1>
+          <h1 className="text-4xl py-4 font-medium">
+            {isSigninForm ? "Sign In" : "Sign Up"}
+          </h1>
+          {!isSigninForm && <input
+            type="text"
+            placeholder="Full Name"
+            className="p-4 my-4 w-full bg-gray-800"
+          />}
           <input
             type="text"
             placeholder="Email"
@@ -25,11 +36,15 @@ const Login = () => {
             className="p-4 my-4 w-full bg-gray-800"
           />
           <button className="p-4 my-6 bg-red-700 w-full rounded-lg">
-            Sign In
+            {isSigninForm ? "Sign In" : "Sign Up"}
           </button>
           <div className="py-0">
-            <span className="text-gray-500">New to Netflix?</span>
-            <span> Sign Up Now.</span>
+            <span className="text-gray-500">
+              {isSigninForm ? "New to Netflix? " : "Already Registered? "}
+            </span>
+            <span onClick={toggleSignInForm} className="cursor-pointer underline">
+              {isSigninForm ? "Sign Up Now." : "Sign In Now"}
+            </span>
           </div>
         </form>
       </div>
