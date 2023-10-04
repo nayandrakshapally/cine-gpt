05cf40babf4ece7a845462b600ce3d040a24a13b
nayandrakshapally <nayan.drakshapally@gmail.com>
2023-10-04 11:44:05 -0400

    added firebase setup for deployment

---
+++ src/utils/firebase.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-unused-vars */
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAxeiBlyuU-yNepiIf-sCdeUQonNmyrEN4",
+  authDomain: "cinegpt-619ed.firebaseapp.com",
+  projectId: "cinegpt-619ed",
+  storageBucket: "cinegpt-619ed.appspot.com",
+  messagingSenderId: "875133507525",
+  appId: "1:875133507525:web:27e66448f734480707ddac",
+  measurementId: "G-272C7QS8M4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
